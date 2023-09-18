755b72026985a8634e9b9a1e7a7fcf00906a37cf
Norman-w <85535885@qq.com>
2023-09-18 16:56:34 +0800

    版本4 更新用户名的业务封装

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -35,6 +37,41 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 	//返回对象
 	return client
+}
+
+// 处理server回应的消息,直接显示到标准输出即可
+func (client *Client) DealResponse() {
+	//等同于创建buf,然后读取buf,再打印到页面上.
+	//一旦client.conn有数据,就直接copy到stdout标准输出上,并且永久阻塞监听
+	i, err := io.Copy(os.Stdout, client.conn)
+	if err != nil {
+		fmt.Println("读取消息失败:", err)
+		fmt.Println("读取消息返回结果:", i)
+		return
+	}
+
+	/*for {
+	buf:
+		make()
+	client.conn.Read(buf)
+	fmt.Printf(buf)
+	}*/
+}
+
+func (client *Client) UpdateName() bool {
+	fmt.Println("请输入用户名")
+	scanned, err := fmt.Scanln(&client.Name)
+	if err != nil {
+		fmt.Println("获取用户输入错误:", scanned)
+		return false
+	}
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err = client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return false
+	}
+	return true
 }
 
 func (client *Client) menu() bool {
@@ -76,7 +113,7 @@ func (client *Client) Run() {
 			break
 		case 3:
 			//修改用户名
-			fmt.Println("修改用户名")
+			client.UpdateName()
 			break
 		}
 	}
@@ -101,6 +138,9 @@ func main() {
 		return
 	}
 	fmt.Println(">>>>>服务器连接成功!!!")
+
+	//单独开启一个goroutine 处理server回传的消息
+	go client.DealResponse()
 
 	//启动客户端业务
 	client.Run()
